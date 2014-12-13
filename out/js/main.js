592ac96a6299b6688d1a5ae0aeed8205e6979b22
Peter Andringa <peter9221@gmail.com>
2014-12-13 16:58:22 -0500

    red covers

--- out/js/main.js
+++ out/js/main.js
@@ -1,34 +1,77 @@
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
+                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+ 
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+ 
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
+
 jQuery(function($){
 
-	$('.photo-1').css('opacity', '1');
+	// $('.photo-1').css('opacity', '1');
 	
-	var current;
+	// var current;
 
-	function switchBackground(num){
-		if(current == num || $('.photo-'+num).length==0) return;
-		console.log("switching from", current, "to", num)
-		$('.photo-'+current).css('opacity', '0');
-		$('.photo-'+num).css('opacity', 1);
+	// function switchBackground(num){
+	// 	if(current == num || $('.photo-'+num).length==0) return;
+	// 	console.log("switching from", current, "to", num)
+	// 	$('.photo-'+current).css('opacity', '0');
+	// 	$('.photo-'+num).css('opacity', 1);
 
-		current = num;
-	}
+	// 	current = num;
+	// }
 
-	var bottoms = [0];
+	// var bottoms = [0];
+	// $('.header').each(function(index, h){
+	// 	bottoms.push($(h).offset().top + $(h).height())
+	// });
+	// console.log(bottoms)
+
+	var sections = [];
 	$('.header').each(function(index, h){
-		bottoms.push($(h).offset().top + $(h).height())
-	});
-	console.log(bottoms)
-	$(window).on('scroll', function(){
-		var s = $(window).scrollTop();
+		var section = {};
 
-		for(var i=0; i<bottoms.length; i++){
-			var top1 = bottoms[i] || 0;
-			var top2 = bottoms[i+1] || 10000000;
-			if( top1 < s && s < top2 ){
-				switchBackground(i+1);
-			}
-		}
-		// if(s > 0) switchBackground( 1 );
-		// else switchBackground( 2 )
+		section.header = $(h);
+		section.content = section.header.next();
+		section.background = $('.'+section.header.attr('data-bg'));
+
+		section.contentMidpoint = section.content.offset().top + section.content.height() / 2;
+		sections.push(section);
+	});
+
+	var h = $(window).height();
+
+	function scrollHandler(){
+		var s = $(window).scrollTop();
+		for(var i=0; i<sections.length; i++){
+			var y = sections[i].contentMidpoint - s; // Midpoint of content
+			y = y <= 0 ? 0
+			  : y >= h ? h
+			  : y;
+			sections[i].background.height(y);
+		};
+		
+	}
+	scrollHandler();
+	$(window).on('scroll', function(e){
+		console.log(e);
+		window.requestAnimationFrame(scrollHandler);
 	});
 })
