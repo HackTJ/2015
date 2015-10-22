51d7472736b1b26a674b9e82fae6d1f79285c019
Peter Andringa <peter@andrin.ga>
2015-10-22 15:58:09 -0400

    refactor gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -9,73 +9,133 @@ var homepageRepo = {
 }
 /* ----------------------------------------------------- */
 
-var gulp = require('gulp');
-var jade = require('gulp-jade');
-var sass = require('gulp-sass');
-var minifyCSS = require('gulp-minify-css')
-var minifyJS = require('gulp-uglify')
-var minifyIMG = require('gulp-imagemin')
-var deploy = require("gulp-gh-pages");
-var static = require('node-static');
 var concat = require('gulp-concat');
+var deploy = require("gulp-gh-pages");
+var gulp = require('gulp');
+var http = require('node-static');
+var jade = require('gulp-jade');
+var minifyIMG = require('gulp-imagemin')
+var minifyJS = require('gulp-uglify')
+var rm = require('gulp-rm');
+var sass = require('gulp-sass');
+var when = require('when');
+
+
+/**** Compiler tasks ****/
+var compiler = {};
+
+// Clean /out directory
+compiler.clean = function() {
+	console.log('Cleaning /out directory...');
+	return gulp.src( './out/**/*', { read: false })
+    .pipe( rm() )
+}
 
 // Compile HTML
-gulp.task('html', function() {
+compiler.html = function() {
+	console.log('Compiling HTML...');
     return gulp.src('./jade/[!_]*.jade')
         .pipe(jade())
         .pipe(gulp.dest('./out'))
-});
+}
 
 // Compile CSS
-gulp.task('css', function () {
+compiler.css = function() {
+	console.log('Compiling CSS...');
     return gulp.src('./scss/[!_]*.scss')
-        .pipe(sass())
+        .pipe(sass({outputStyle: 'compressed'}))
         .on('error', function (err) { console.log(err.message); })
-        .pipe(minifyCSS())
         .pipe(gulp.dest('./out/css'))
-});
+}
 
-// Copy js
-gulp.task('js', function(){
+// Compile JS
+compiler.js = function() {
+	console.log('Compiling JS...');
     return gulp.src(['./js/_*.js', './js/*.js'])
         .pipe(concat('main.js'))
         .pipe(minifyJS())
         .pipe(gulp.dest('./out/js'))
-});
+}
 
-// Copy over static resources
-gulp.task('static', function(){
+// Compile static resources
+compiler.static = function(){
+	console.log('Compiling static...');
     return gulp.src('./static/**')
         .pipe(gulp.dest('./out'));
-});
+}
 
-// Compress images - used only for deploys
-gulp.task('static-images', function(){
+compiler.static_compression = function(){
+	console.log('Compressing static...')
     return gulp.src('./static/**')
         .pipe(minifyIMG({
             optimizationLevel: 5
         }))
         .pipe(gulp.dest('./out'));
-});
+}
 
-// Deploy tasks
-gulp.task('deploy', ['css', 'html', 'static-images', 'js'], function () {
-    return gulp.src("./out/**/*")
+// Add gulp tasks for each compiler
+Object.keys(compiler).forEach(function(name){
+	gulp.task(name, compiler[name]);
+})
+
+// Clear directory and compile all
+var compileAll = function(){
+	return when.all([
+		compiler.html(),
+		compiler.css(),
+		compiler.js(),
+		compiler.static_compression()
+	])
+}
+gulp.task('compile', ['clean'], compileAll)
+
+var compileDev = function(cb){
+	return when.all([
+		compiler.html(),
+		compiler.css(),
+		compiler.js(),
+		compiler.static()
+	])
+}
+gulp.task('compile-dev', ['clean'], compileDev)
+
+
+/**** Deploy tasks ****/
+var deploy = {}
+
+// Deploy to hacktj.org/year
+deploy.year = function(){
+	console.log("Deploying to event page")
+	return gulp.src("./out/**/*")
         .pipe( deploy( repo ) )
-        .pipe( deploy() );
-});
-gulp.task('deploy-homepage', ['css', 'html', 'static-images', 'js'], function () {
+}
+// Deploy to hacktj.org
+deploy.homepage = function() {
+	console.log("Deploying to homepage")
     return gulp.src("./out/**/*")
         .pipe( deploy( homepageRepo ) )
-        .pipe( deploy() );
+}
+
+// Deploy to both targets
+deploy.all = function(cb){
+	return when.all([
+		deploy.year,
+	 	deploy.homepage
+	])
+}
+
+// Add gulp tasks for each deploy target
+Object.keys(compiler).forEach(function(target){
+	gulp.task('deploy-'+target, ['compile'], deploy[target]);
 });
-gulp.task('deploy-all', ['deploy', 'deploy-homepage']);
+
 
 gulp.task('watch', ['default'], function() {
+
     port = (process.argv.length > 4 && process.argv[3] == '--port') ? parseInt(process.argv[4]) : 8000;
     require('http').createServer(function (request, response) {
         request.addListener('end', function () {
-            new static.Server('./out').serve(request, response);
+            new http.Server('./out').serve(request, response);
         }).resume();
     }).listen(port);
 
@@ -87,4 +147,4 @@ gulp.task('watch', ['default'], function() {
     console.log("Server listening on port %s", port)
 });
 
-gulp.task('default', ['css', 'html', 'js', 'static'])
+gulp.task('default', ['compile-dev'])
